2f63fa07ff420035a6a672ad5de6880c3c882075
Curtis Maloney <curtis@tinbrain.net>
2017-03-15 14:44:14 +1100

    Return a promise

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ class Assets {
           'deploy'
         ],
         options: {
-            verbose: {
-                usage: "Increase verbosity",
-                shortcut: 'v'
-            }
+          verbose: {
+            usage: "Increase verbosity",
+            shortcut: 'v'
+          }
         }
       }
     };
@@ -40,26 +40,27 @@ class Assets {
     const config = service.custom.assets;
 
     // glob
-    config.files.forEach((opt) => {
-      let cfg = Object.assign({}, globOpts, {cwd: opt.source});
-      glob.sync(opt.globs, cfg).forEach((fn) => {
+    return new Promise((resolve, reject) => {
+      config.files.forEach((opt) => {
+        let cfg = Object.assign({}, globOpts, {cwd: opt.source});
+        glob.sync(opt.globs, cfg).forEach((fn) => {
 
-        const body = fs.readFileSync(opt.source + fn)
-        const type = mime.lookup(fn);
+          const body = fs.readFileSync(opt.source + fn)
+          const type = mime.lookup(fn);
 
-        (!!this.options.verbose) && this.serverless.cli.log("File: ", fn, type)
+          (!!this.options.verbose) && this.serverless.cli.log("File: ", fn, type)
 
-        this.provider.request('S3', 'putObject', {
-          ACL: config.acl || 'public-read',
-          Body: body,
-          Bucket: config.bucket,
-          Key: fn,
-          ContentType: type
-        }, this.options.stage, this.options.region);
+          this.provider.request('S3', 'putObject', {
+            ACL: config.acl || 'public-read',
+            Body: body,
+            Bucket: config.bucket,
+            Key: fn,
+            ContentType: type
+          }, this.options.stage, this.options.region);
 
-      });
+        });
     });
-
+    resolve();
   }
 }
 
