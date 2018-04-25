e9ac7df40010218a26909a33c021fdcd9b4eb2c2
Jonas Holtorf <Jonas.Holtorf@4wits.de>
2018-04-25 22:07:49 +0200

    fix windows key issue. add feat to empty bucket before upload

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-'use strict';
+'use strict'
 
-const glob = require('glob-all');
-const fs = require('fs');
-const path = require('path');
-const mime = require('mime-types');
-const BbPromise = require('bluebird');
+const glob = require('glob-all')
+const fs = require('fs')
+const path = require('path')
+const mime = require('mime-types')
+const BbPromise = require('bluebird')
 
 const globOpts = {
   nodir: true
-};
-
+}
 
 class Assets {
-  constructor (serverless, options) {
-    this.serverless = serverless;
-    this.options = options;
-    this.provider = this.serverless.getProvider('aws');
+  constructor(serverless, options) {
+    this.serverless = serverless
+    this.options = options
+    this.provider = this.serverless.getProvider('aws')
 
-    let config = this.serverless.service.custom.assets;
-    if(Array.isArray(config)) {
-      config = {targets: config};
+    let config = this.serverless.service.custom.assets
+    if (Array.isArray(config)) {
+      config = { targets: config }
     }
 
-    this.config = Object.assign({}, {
-      auto: false,
-      targets: [],
-    }, config);
+    this.config = Object.assign(
+      {},
+      {
+        auto: false,
+        targets: []
+      },
+      config
+    )
 
     this.commands = {
       s3deploy: {
         usage: 'Deploy assets to S3 bucket',
-        lifecycleEvents: [
-          'deploy'
-        ],
+        lifecycleEvents: ['deploy'],
         options: {
           verbose: {
             usage: 'Increase verbosity',
@@ -44,110 +45,162 @@ class Assets {
           }
         }
       }
-    };
+    }
 
     this.hooks = {
       's3deploy:deploy': () => Promise.resolve().then(this.deployS3.bind(this)),
-      'after:deploy:finalize': () => Promise.resolve().then(this.afterDeploy.bind(this))
-    };
+      'after:deploy:finalize': () =>
+        Promise.resolve().then(this.afterDeploy.bind(this))
+    }
   }
 
   /*
    * Handy method for logging (when `verbose` is set)
    */
   log(message) {
-    if(this.options.verbose) {
-      this.serverless.cli.log(message);
+    if (this.options.verbose || process.env.SLS_DEBUG) {
+      this.serverless.cli.log(message)
     }
   }
 
   afterDeploy() {
-    if(this.config.auto) {
-      this.deployS3();
+    if (this.config.auto) {
+      this.deployS3()
     }
   }
 
   listStackResources(resources, nextToken) {
-    resources = resources || [];
-    return this.provider.request('CloudFormation', 'listStackResources', { StackName: this.provider.naming.getStackName(), NextToken: nextToken })
-    .then(response => {
-      resources.push.apply(resources, response.StackResourceSummaries);
-      if (response.NextToken) {
-        // Query next page
-        return this.listStackResources(resources, response.NextToken);
-      }
-    })
-    .return(resources);
+    resources = resources || []
+    return this.provider
+      .request('CloudFormation', 'listStackResources', {
+        StackName: this.provider.naming.getStackName(),
+        NextToken: nextToken
+      })
+      .then((response) => {
+        resources.push.apply(resources, response.StackResourceSummaries)
+        if (response.NextToken) {
+          // Query next page
+          return this.listStackResources(resources, response.NextToken)
+        }
+      })
+      .return(resources)
   }
-  
+
   resolveBucket(resources, value) {
     if (typeof value === 'string') {
-      return BbPromise.resolve(value);
-    }
-    else if (value && value.Ref) {
-      let resolved;
-      resources.forEach(resource => {
+      return BbPromise.resolve(value)
+    } else if (value && value.Ref) {
+      let resolved
+      resources.forEach((resource) => {
         if (resource && resource.LogicalResourceId === value.Ref) {
-          resolved = resource.PhysicalResourceId;
+          resolved = resource.PhysicalResourceId
         }
-      });
+      })
 
       if (!resolved) {
-        this.serverless.cli.log(`WARNING: Failed to resolve reference ${value.Ref}`);
+        this.serverless.cli.log(
+          `WARNING: Failed to resolve reference ${value.Ref}`
+        )
       }
-      return BbPromise.resolve(resolved);
+      return BbPromise.resolve(resolved)
+    } else {
+      return BbPromise.reject(new Error(`Invalid bucket name ${value}`))
     }
-    else {
-      return BbPromise.reject(new Error(`Invalid bucket name ${value}`));
+  }
+
+  emptyBucket(bucket, dir) {
+    const listParams = {
+      Bucket: bucket,
+      Prefix: dir
     }
+
+    return this.provider.request('S3', 'listObjectsV2', listParams)
+      .then((listedObjects) => {
+        if (listedObjects.Contents.length === 0) return
+
+        const deleteParams = {
+          Bucket: bucket,
+          Delete: { Objects: [] }
+        }
+
+        listedObjects.Contents.forEach(({ Key }) => {
+          deleteParams.Delete.Objects.push({ Key })
+        })
+        return this.provider.request('S3', 'deleteObjects', deleteParams)
+          .then(() => {
+            if (listedObjects.Contents.IsTruncated) {
+              this.log(`Is not finished. Rerun emptyBucket`)
+              return this.emptyBucket(bucket, dir)
+            }
+          })
+      })
   }
 
   deployS3() {
-    let assetSets = this.config.targets;
+    let assetSets = this.config.targets
 
     // Read existing stack resources so we can resolve references if necessary
-    return this.listStackResources()
-    .then(resources => {
+    return this.listStackResources().then((resources) => {
       // Process asset sets in parallel (up to 3)
-      return BbPromise.map(assetSets, assets => {
-        // Try to resolve the bucket name
-        return this.resolveBucket(resources, assets.bucket)
-        .then(bucket => {
-          const prefix = assets.prefix || '';
-          // Process files serially to not overload the network
-          return BbPromise.each(assets.files, opt => {
-            this.log(`Bucket: ${bucket}:${prefix}`);
-
-            if(this.options.bucket && this.options.bucket !== bucket) {
-              this.log('Skipping');
-              return;
+      return BbPromise.map(
+        assetSets,
+        (assets) => {
+          const prefix = assets.prefix || ''
+          // Try to resolve the bucket name
+          return this.resolveBucket(resources, assets.bucket).then((bucket) => {
+            if(assets.empty) {
+              this.log(`Emptying bucket`)
+              return this.emptyBucket(bucket, prefix)
+                .then(() => bucket)
             }
+            return Promise.resolve(bucket)
+          }).then(bucket => {
+            // Process files serially to not overload the network
+            return BbPromise.each(assets.files, (opt) => {
+              this.log(`Bucket: ${bucket}:${prefix}`)
 
-            this.log(`Path: ${opt.source}`);
+              if (this.options.bucket && this.options.bucket !== bucket) {
+                this.log('Skipping')
+                return
+              }
 
-            const cfg = Object.assign({}, globOpts, {cwd: opt.source});
-            glob.sync(opt.globs, cfg).forEach(filename => {
+              this.log(`Path: ${opt.source}`)
 
-              const body = fs.readFileSync(path.join(opt.source, filename));
-              const type = mime.lookup(filename) || opt.defaultContentType || 'application/octet-stream';
+              const cfg = Object.assign({}, globOpts, { cwd: opt.source })
+              glob.sync(opt.globs, cfg).forEach((filename) => {
+                const body = fs.readFileSync(path.join(opt.source, filename))
+                const type =
+                  mime.lookup(filename) ||
+                  opt.defaultContentType ||
+                  'application/octet-stream'
 
-              this.log(`\tFile:  ${filename} (${type})`);
+                this.log(`\tFile:  ${filename} (${type})`)
 
-              const details = Object.assign({
-                ACL: assets.acl || 'public-read',
-                Body: body,
-                Bucket: bucket,
-                Key: path.join(prefix, filename),
-                ContentType: type
-              }, opt.headers || {});
+                const key = path
+                  .join(prefix, filename)
+                  .split('\\')
+                  .join('/')
 
-              return this.provider.request('S3', 'putObject', details);
-            });
-          });
-        });
-      }, { concurrency: 3 });
-    });
+                const details = Object.assign(
+                  {
+                    ACL: assets.acl || 'public-read',
+                    Body: body,
+                    Bucket: bucket,
+                    Key: key,
+                    ContentType: type
+                  },
+                  opt.headers || {}
+                )
+
+                return this.provider.request('S3', 'putObject', details)
+              })
+            })
+          })
+        },
+        { concurrency: 3 }
+      )
+    })
   }
 }
 
-module.exports = Assets;
+module.exports = Assets
