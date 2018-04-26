a6aa0c16865ed17402385dcb1531d349421b89a8
Jonas <jonasho@users.noreply.github.com>
2018-04-26 13:51:35 +0200

    [Fixes #15 #18] Fix Windows issue. Empty feature added.
    * fix windows key issue. add feat to empty bucket before upload

--- index.js
+++ index.js
@@ -54,9 +54,10 @@ class Assets {
 
   /*
    * Handy method for logging (when `verbose` is set)
+   * Also log on the default serverless SLS_DEBUG env
    */
   log(message) {
-    if(this.options.verbose) {
+    if(this.options.verbose || process.env.SLS_DEBUG) {
       this.serverless.cli.log(message);
     }
   }
@@ -79,7 +80,7 @@ class Assets {
     })
     .return(resources);
   }
-  
+
   resolveBucket(resources, value) {
     if (typeof value === 'string') {
       return BbPromise.resolve(value);
@@ -102,6 +103,34 @@ class Assets {
     }
   }
 
+  emptyBucket(bucket, dir) {
+    const listParams = {
+      Bucket: bucket,
+      Prefix: dir
+    };
+
+    return this.provider.request('S3', 'listObjectsV2', listParams)
+      .then((listedObjects) => {
+        if (listedObjects.Contents.length === 0) return
+
+        const deleteParams = {
+          Bucket: bucket,
+          Delete: { Objects: [] }
+        };
+
+        listedObjects.Contents.forEach(({ Key }) => {
+          deleteParams.Delete.Objects.push({ Key })
+        });
+        return this.provider.request('S3', 'deleteObjects', deleteParams)
+          .then(() => {
+            if (listedObjects.Contents.IsTruncated) {
+              this.log(`Is not finished. Rerun emptyBucket`)
+              return this.emptyBucket(bucket, dir)
+            }
+          })
+      });
+  }
+
   deployS3() {
     let assetSets = this.config.targets;
 
@@ -110,43 +139,60 @@ class Assets {
     .then(resources => {
       // Process asset sets in parallel (up to 3)
       return BbPromise.map(assetSets, assets => {
-        // Try to resolve the bucket name
-        return this.resolveBucket(resources, assets.bucket)
-        .then(bucket => {
           const prefix = assets.prefix || '';
-          // Process files serially to not overload the network
-          return BbPromise.each(assets.files, opt => {
-            this.log(`Bucket: ${bucket}:${prefix}`);
-
-            if(this.options.bucket && this.options.bucket !== bucket) {
-              this.log('Skipping');
-              return;
+          // Try to resolve the bucket name
+          return this.resolveBucket(resources, assets.bucket)
+          .then((bucket) => {
+            if(assets.empty) {
+              this.log(`Emptying bucket`)
+              return this.emptyBucket(bucket, prefix)
+                .then(() => bucket)
             }
+            return Promise.resolve(bucket)
+          }).then(bucket => {
+            // Process files serially to not overload the network
+            return BbPromise.each(assets.files, (opt) => {
+              this.log(`Bucket: ${bucket}:${prefix}`)
 
-            this.log(`Path: ${opt.source}`);
+              if (this.options.bucket && this.options.bucket !== bucket) {
+                this.log('Skipping')
+                return
+              }
 
-            const cfg = Object.assign({}, globOpts, {cwd: opt.source});
-            glob.sync(opt.globs, cfg).forEach(filename => {
+              this.log(`Path: ${opt.source}`)
 
-              const body = fs.readFileSync(path.join(opt.source, filename));
-              const type = mime.lookup(filename) || opt.defaultContentType || 'application/octet-stream';
+              const cfg = Object.assign({}, globOpts, { cwd: opt.source })
+              glob.sync(opt.globs, cfg).forEach((filename) => {
+                const body = fs.readFileSync(path.join(opt.source, filename))
+                const type = mime.lookup(filename) || opt.defaultContentType || 'application/octet-stream'
 
-              this.log(`\tFile:  ${filename} (${type})`);
+                this.log(`\tFile:  ${filename} (${type})`);
 
-              const details = Object.assign({
-                ACL: assets.acl || 'public-read',
-                Body: body,
-                Bucket: bucket,
-                Key: path.join(prefix, filename),
-                ContentType: type
-              }, opt.headers || {});
+                // when using windows path join resolves to backslashes, but s3 is expecting a slash
+                // therefore replace all backslashes with slashes
+                const key = path
+                  .join(prefix, filename)
+                  .replace(/\\/g, '/');
 
-              return this.provider.request('S3', 'putObject', details);
-            });
-          });
-        });
-      }, { concurrency: 3 });
-    });
+                const details = Object.assign(
+                  {
+                    ACL: assets.acl || 'public-read',
+                    Body: body,
+                    Bucket: bucket,
+                    Key: key,
+                    ContentType: type
+                  },
+                  opt.headers || {}
+                );
+
+                return this.provider.request('S3', 'putObject', details)
+              })
+            })
+          })
+        },
+        { concurrency: 3 }
+      )
+    })
   }
 }
 
